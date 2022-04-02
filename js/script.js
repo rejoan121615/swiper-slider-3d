27d65778d6c39c53d079282d524b16aafd2bf3cd
Mohd Rejoan <wearecrx@gmail.com>
2022-04-02 15:57:48 +0600

    select disable , left right button replaced, loop added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
+    loop: true,
     autoplay: {
+        // here is the time
         delay: 5000,
-  },
+    },
     speed: 500,
     navigation: {
         nextEl: ".swiper-button-next",
